30deab5ff319edf2d8526e29f70ed0ec4380d45c
Nsanawaji <nsanawajiadasi@gmail.com>
2024-06-20 15:06:17 +0100

    Adding Create user method in the userservice

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from '../dtos/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  constructor(
-    @InjectRepository(User) private userRepo: Repository<User>) {}
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
 
-    async createUser(){
-        
+  async createUser(payload: CreateUserDto) {
+    const { email, password, ...rest } = payload;
+    const isUser = await this.userRepo.findOneBy({ email });
+    if (isUser) {
+      throw new HttpException(
+        `User with this email: ${email} already exist`,
+        400,
+      );
     }
 
+    //encrypt password
+    const hashpassword = await bcrypt.hash(password, 12);
+
+    try {
+      const user = await this.userRepo.save({
+        email,
+        password: hashpassword,
+        ...rest,
+      });
+      delete user.password;
+      console.log('User successfully craeted');
+      return user;
+    } catch (error) {
+      if (error.code === '22P02') {
+        throw new BadRequestException('admin role should be lowercase');
+      }
+      return error;
+    }
+  }
 }
