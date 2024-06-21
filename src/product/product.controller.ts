f55b32b52d76fe4eba654609109736385d3e8dc0
Nsanawaji <nsanawajiadasi@gmail.com>
2024-06-21 14:51:06 +0100

    Adding more methods

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,14 +1,45 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from 'src/dtos/create-product..dto';
+import { UpdateProductDto } from 'src/dtos/update-product.dto';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post()
-  async addProduct(@Body()payload: CreateProductDto){
-    await this.productService.addProduct(payload)
+  async addProduct(@Body() payload: CreateProductDto) {
+    return await this.productService.addProduct(payload);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    return await this.productService.findOneProduct(id);
+  }
+
+  @Get()
+  async findAll() {
+    return await this.productService.findAllProducts();
+  }
+
+  @Put(':id')
+  async updateProduct(
+    @Param('id') id: number,
+    @Body() payload: UpdateProductDto,
+  ) {
+    return await this.productService.updateProduct(id, payload);
+  }
+
+  @Delete(':id')
+  async deleteProduct(@Param('id') id: number) {
+    return await this.productService.deleteProduct(id);
   }
 }
- 
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateProductDto } from 'src/dtos/create-product..dto';
+import { UpdateProductDto } from 'src/dtos/update-product.dto';
 import { Product } from 'src/entities/product.entity';
 import { Repository } from 'typeorm';
 
@@ -10,22 +11,56 @@ export class ProductService {
     @InjectRepository(Product) private ProductRepo: Repository<Product>,
   ) {}
 
+  //function that searches for a product by id
+  async findItems(id: number) {
+    const findPro = await this.ProductRepo.findOneBy({ id: id });
+    if (!findPro) {
+      throw new HttpException('Sorry no such product found', 400);
+    }
+
+    return findPro;
+  }
+  //Add new product
   async addProduct(payload: CreateProductDto) {
-    const product = await this.ProductRepo.save(payload);
-    return product;
+    const { name, ...rest } = payload;
+    const isProduct = await this.ProductRepo.findOneBy({ name });
+    if (isProduct) {
+      throw new HttpException('Product already exist', 401);
+    }
+    try {
+      const product = await this.ProductRepo.save(payload);
+      return payload;
+    } catch (error) {
+      return error;
+    }
   }
 
+  //Find one product by id
   async findOneProduct(id) {
+    return await this.findItems(id) 
+  }
+
+  //update a product
+  async updateProduct(id, payload: UpdateProductDto) {
+    const isProduct = await this.findItems(id)
+    return await this.ProductRepo.update(id, payload);
+    
+  }
+
+  //Find all products
+  async findAllProducts() {
+    const allProducts = await this.ProductRepo.find();
+    if (!allProducts) {
+      throw new HttpException('No products found', 400);
+    }
+    return allProducts;
+  }
+
+  //delete a product
+  async deleteProduct(id) {
     const isProduct = await this.ProductRepo.findOne({ where: { id: id } });
-    return isProduct;
-  }
-
-  async updateProduct(){
-
-  }
-
-  async findAllProducts(){
-    const allProducts = await this.ProductRepo.find()
-    return allProducts 
+    if (isProduct) {
+      return await this.ProductRepo.delete(id);
+    }
   }
 }
