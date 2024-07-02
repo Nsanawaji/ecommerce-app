a3e0be3e41e5fe3a99fe552e1f2666c0a9183de2
Nsanawaji <nsanawajiadasi@gmail.com>
2024-07-02 15:56:42 +0100

    Define category entity

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,6 +1,15 @@
-import { Entity } from "typeorm";
+import { Base } from "src/entities/base.entity";
+import { Product } from "src/entities/product.entity";
+import { Column, Entity, OneToMany } from "typeorm";
 
 @Entity()
-export class Category {
+export class Category extends Base {
+  @Column()
+  name: string;
 
+  @Column({ default: '' })
+  description: string;
+
+  @OneToMany(() => Product, (product) => product.category)
+  products: Product[];
 }
